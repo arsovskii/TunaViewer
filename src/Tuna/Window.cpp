7e8163babd76c430783fe4ad8d30b6ee24a422fb
David <arsovskidavid4@gmail.com>
2024-02-28 13:21:04 +0100

    update opengl

--- src/Tuna/Window.cpp
+++ src/Tuna/Window.cpp
@@ -49,8 +49,8 @@ bool Window::InitGLFW()
 		return false;
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	return true;
